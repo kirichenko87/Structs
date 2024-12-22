3aefe0408dbcc7ef90d09807e3fca91a3582df09
Kirichenko <kappa134rus@gmail.com>
2024-12-22 22:14:47 +0300

    Добавил метод insert_position

--- src/linked_list.py
+++ src/linked_list.py
@@ -34,14 +34,37 @@ class LinkedList:
 
 
     def is_empty(self) -> bool:
-        return False if self.size == 0 else True
+        return True if self.size == 0 else False
 
 
     def clear(self) -> None:
         self.head = None
         self.size = 0
 
+    def insert_position(self, elem: any, position: int ) -> None:
+        node = Node(data=elem, next=None)
 
+        if position > self.size or position < 0:
+            raise ValueError
+
+        if position == 1 and self.size == 0:
+            self.add_head(node)
+            self.size += 1
+
+        elif position == 1 and self.size > 0:
+            self.add_head(node)
+            self.size += 1
+
+        else:
+            iterator = self.head
+            count = 0
+            while count != position -1:
+                iterator = iterator.next
+                count += 1
+
+        node.next = iterator.next
+        iterator.next = node
+        self.size += 1
 
 ll = LinkedList()
 
@@ -57,3 +80,6 @@ ll.add_head(123)
 ll.add_head(1234)
 ll.add_head("hi")
 print(ll.size)
+
+
+ll.insert_position('Hi....', 2)
