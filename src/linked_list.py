55a1fbc3d5cb15c1b357dfc16b3c2a4bc689b34f
Kirichenko <kappa134rus@gmail.com>
2024-12-22 18:03:37 +0300

    feat Добавил функцию добавления в начало структуры

--- src/linked_list.py
+++ src/linked_list.py
@@ -14,16 +14,40 @@ class LinkedList:
     def add(self, elem):
         node = Node(data=elem, next=None)
 
-        if self.head == None:
+        if self.size == 0:
             self.head = node
+            self.size += 1
+        else:
+            iterator = self.head
+            while iterator.next != None:
+                iterator = iterator.next
 
-        iterator = self.head
-        while iterator.next != None:
-            iterator = iterator.next
+            iterator.next = node
+            self.size += 1
 
-        iterator.next = node
-        self.size += 1
+    def add_head(self, elem: any) -> None:
+        node = Node(data=elem, next=None)
+
+        if self.size == 0:
+            self.head = node
+            self.size += 1
+        else:
+            head_link = self.head
+            self.head = node
+            self.head.next = head_link
+            self.size += 1
 
 
 
+ll = LinkedList()
 
+ll.add(11)
+ll.add(12)
+ll.add(13)
+print(ll.size)
+
+ll.add_head(11)
+ll.add_head(123)
+ll.add_head(1234)
+ll.add_head("hi")
+print(ll.size)
